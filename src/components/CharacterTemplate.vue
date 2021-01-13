e06bf8cc04b6782296c302ae46ff1b46ddab28bb
Spiridonovas,Edgaras E <e.spiridonovas@student.fontys.nl>
2021-01-13 06:27:02 +0100

    CSS comments

--- src/components/CharacterTemplate.vue
+++ src/components/CharacterTemplate.vue
@@ -2,18 +2,20 @@
   <router-link
     :to="{ name: 'Character', params: { char_id: character.char_id } }"
   >
-    <img :src="character.img" :alt="character.name" class="character__img" />
-    <span
-      class="character__label"
-      :class="{ 'character__label--alive': character.status === 'Alive' }"
-      >{{ character.status }}</span
-    >
-    <h3 class="character__title">{{ character.name }}</h3>
-    <div class="viewcase-container">
-      <img src="@/assets/icons/blue-arrow.svg" alt="" />
-      <span class="character__button">View character</span>
+    <div class="charcter-wrapper">
+      <img :src="character.img" :alt="character.name" class="character__img" />
+      <span
+        class="character__label"
+        :class="{ 'character__label--alive': character.status === 'Alive' }"
+        >{{ character.status }}</span
+      >
+      <h3 class="character__title">{{ character.name }}</h3>
+      <div class="viewcase-container">
+        <img src="@/assets/icons/blue-arrow.svg" alt="" />
+        <span class="character__button">View character</span>
+      </div>
+      <!-- / viewcase-container -->
     </div>
-    <!-- / viewcase-container -->
   </router-link>
 </template>
 
